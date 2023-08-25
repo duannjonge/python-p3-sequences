dfa0502dea40d580777598970897b1b73f6d0f54
duan spot <brandennjonge@gmail.com>
2023-08-25 03:21:03 +0300

    completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    nums = []  # Initialize an empty list to store the Fibonacci sequence
+    a, b = 0, 1  # Initialize the first two Fibonacci numbers
+
+    for _ in range(length):
+        nums.append(a)  # Add the current Fibonacci number to the list
+        a, b = b, a + b  # Calculate the next Fibonacci number
+
+    print(nums)
+
+print_fibonacci(9)
